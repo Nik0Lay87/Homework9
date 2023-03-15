eaa1b4a16255fbf961e796e0d3d8a467caedbf74
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-03-15 10:08:18 +0300

    Добавили папку Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int numberM = 1;
+int numberN = 15;
+SumFromMToN(numberM, numberN, 0);
+
+void SumFromMToN (int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"{sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    SumFromMToN(numberM, numberN, sum);
+}
