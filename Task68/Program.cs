89a942dfffed5d12d1dae63f31f0ec071199a01e
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-03-15 10:08:31 +0300

    Добавили папку Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+
+int m = 2;
+int n = 3;
+
+int functionAck = Ack(m, n);
+Console.Write($"{functionAck} ");
+
+int Ack(int m, int n)
+{
+    if(m==0) return n + 1;
+    else if((m != 0) && (n == 0)) return Ack(m - 1, 1);
+    else return Ack(m - 1, Ack(m,n - 1)); 
+}
+
